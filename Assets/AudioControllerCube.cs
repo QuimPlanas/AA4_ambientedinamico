ae33f503969e09177b64715ffd82be6055629bed
QuimPlanas <84715023+QuimPlanas@users.noreply.github.com>
2021-05-24 20:10:58 +0200

    Add files via upload

---
+++ Assets/AudioControllerCube.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControllerCube : MonoBehaviour
+{
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        Renderer rend = GetComponent<Renderer>();
+        Bounds bounds = rend.bounds;
+        source.pitch = 1.5f / bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // métod llamada por Unity cuando chocamos con algo ...
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        source.Play();
+    }
+
+}
